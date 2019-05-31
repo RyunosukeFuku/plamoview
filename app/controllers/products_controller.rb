8f9dd90c84aff092e4763fd3ebdd3fae7597a967
RyunosukeFuku <rfutian64@gmail.com>
2019-06-01 04:43:22 +0900

    edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.new(product_params)
+    @product = Product.create(product_params)
     if @product.save
        redirect_to root_path ,notice: '記事が投稿されました'
      else
